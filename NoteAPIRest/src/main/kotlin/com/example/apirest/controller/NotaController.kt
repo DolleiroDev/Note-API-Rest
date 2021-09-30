5c3e7670d5b85d71687473381d9bcf2d66393725
DolleiroDev <66499462+DolleiroDev@users.noreply.github.com>
2021-09-30 15:51:57 -0300

    Adicionado métodos de Delete e Put

--- NoteAPIRest/src/main/kotlin/com/example/apirest/controller/NotaController.kt
+++ NoteAPIRest/src/main/kotlin/com/example/apirest/controller/NotaController.kt
@@ -7,10 +7,7 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("notes")
-class NotaController {
-
-    @Autowired
-    lateinit var noteRepository: NoteRepository
+class NotaController(private val noteRepository: NoteRepository){
 
     @PostMapping()
     fun create(@RequestBody note: Note): Note {
@@ -19,5 +16,19 @@ class NotaController {
     @GetMapping
     fun read(): List<Note> {
         return noteRepository.findAll().toList()
+    }
+    @PutMapping("{id}")
+    fun update(@PathVariable id: Long, @RequestBody note: Note) : Note {
+        if (noteRepository.existsById(id)) {
+            val safeNote = note.copy(id)
+            return noteRepository.save(safeNote)
+        }
+        return Note()
+    }
+    @DeleteMapping("{id}")
+    fun delete(@PathVariable id: Long) {
+        if (noteRepository.existsById(id)) {
+            noteRepository.deleteById(id)
+        }
     }
 }
